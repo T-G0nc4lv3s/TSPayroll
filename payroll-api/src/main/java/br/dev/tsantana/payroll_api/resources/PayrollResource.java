8cd3c1ecc0396abbaa09fcb765b222dd2e9e1c71
Thiago Gonçalves <mg.az.ul@hotmail.com>
2023-05-23 12:00:42 -0300

    PayrollService refactor, service-level exception handling

--- payroll-api/src/main/java/br/dev/tsantana/payroll_api/resources/PayrollResource.java
+++ payroll-api/src/main/java/br/dev/tsantana/payroll_api/resources/PayrollResource.java
@@ -1,6 +1,5 @@
 package br.dev.tsantana.payroll_api.resources;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,26 +8,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.dev.tsantana.payroll_api.domain.Payroll;
-import br.dev.tsantana.payroll_api.dto.UserDTO;
-import br.dev.tsantana.payroll_api.feignClients.UserFeign;
+import br.dev.tsantana.payroll_api.services.PayrollService;
+import lombok.RequiredArgsConstructor;
 
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/api/payments")
 public class PayrollResource {
 	
-	@Autowired
-	private UserFeign userFeign;
+	private final PayrollService service;
 
 	@GetMapping(value = "/{workerId}")
 	public ResponseEntity<Payroll> getPayment(@PathVariable Long workerId, @RequestBody Payroll payment){
-		UserDTO user = userFeign.findById(workerId).getBody();
-		return ResponseEntity.ok(new Payroll(
-				user.getName(),
-				payment.getDescription(),
-				user.getHourlyPrice(),
-				payment.getWorkedHours(),
-				user.getHourlyPrice() * payment.getWorkedHours()
-				));
+		return ResponseEntity.ok().body(service.getPayment(workerId, payment));
 	}
 }
---
+++ payroll-api/src/main/java/br/dev/tsantana/payroll_api/resources/exceptions/StandardError.java
@@ -0,0 +1,18 @@
+package br.dev.tsantana.payroll_api.resources.exceptions;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class StandardError {
+
+	private LocalDateTime timestamp;
+    private Integer status;
+    private String error;
+    private String path;
+}
