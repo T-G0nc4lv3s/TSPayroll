30bdd32cc055e72af5fd2dd6c05e2e0892625f71
Thiago Gonçalves <mg.az.ul@hotmail.com>
2023-05-24 15:13:21 -0300

    docker-compose file

--- user-api-8003/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO TB_USER (name, email, password, hourly_price) VALUES ('Bob', 'bob@gmail.com', '12345', 20.0);
-INSERT INTO TB_USER (name, email, password, hourly_Price) VALUES ('Ana', 'ana@gmail.com', '12345', 30.0);
-INSERT INTO TB_USER (name, email, password, hourly_Price) VALUES ('Alex', 'alex@gmail.com', '12345', 25.50);
